1f34045eb7dc08680ec13d318061dede462706f6
gutie <gutie@LAPTOP-JM1T2PON>
2019-11-13 19:53:25 -0600

    hola

--- Multilenguaje/Multilenguaje/MainWindow.xaml.cs
+++ Multilenguaje/Multilenguaje/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Resources;
 
 namespace Multilenguaje
 {
@@ -20,9 +21,43 @@ namespace Multilenguaje
     /// </summary>
     public partial class MainWindow : Window
     {
+        string strLenguage = "";
+   
         public MainWindow()
         {
+           
             InitializeComponent();
+           
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Set_Lenguage();
+        }
+        private void ddlLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Set_Lenguage();
+        }
+        private void Set_Lenguage()
+        {
+            strLenguage = "Multilenguaje.Lenguajes." + ((ComboBoxItem)ddlLanguaje.SelectedItem).Name.ToString();
+            ResourceManager LocRM = new ResourceManager(strLenguage, typeof(MainWindow).Assembly);
+            lblAge.Text = LocRM.GetString("strAge");
+            lblContact.Text = LocRM.GetString("strContact");
+            lblFirstName.Text = LocRM.GetString("strFirstName");
+            lblLastName.Text = LocRM.GetString("strLastName");
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ddlLanguaje_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
